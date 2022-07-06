14b04e2df0090f25222081b844491f0e2941366c
Richard Yen <richyen3@gmail.com>
2022-07-06 15:16:18 -0700

    Update comments for clarity

--- stream_iconik_mapping/rename_collections.sh
+++ stream_iconik_mapping/rename_collections.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 # This script will rename collection titles to their proper collection name
-# This script is necessasry because in the MS Stream export, channels were exported by UUID, not by title
+# This script is necessary because in the MS Stream export, channels were exported by UUID, not by title
 # This script will use the channel-to-uuid mapping and rename the collection to match the channel name
 
-for c in `cat collections.txt`; do
+# collections.csv was created externally, and has the format:
+# Column 1 -- MS Stream Channel UUID
+# Column 2 -- Iconik Collection UUID
+# Column 3 -- MS Stream Channel Title
+# Note the title had spaces replaced with "____" to work around bash's default IFS/delimiter, which is a space
+
+for c in `cat collections.csv`; do
 
   IFS=',' read -r -a array <<< "$c"
-  collection_id=${array[0]}
-  new_title=`echo ${array[1]} | sed "s/____/ /g"`
+  ms_stream_id=${array[0]}
+  collection_id=${array[1]}
+  new_title=`echo ${array[2]} | sed "s/____/ /g"`
 
   echo "Going to rename ${collection_id} to ${new_title}"
   curl -sX PUT "https://app.iconik.io/API/assets/v1/collections/${collection_id}/" -H "accept: application/json" -H "App-ID: ${ICONIK_APP_ID}" -H "Auth-Token: ${ICONIK_APP_TOKEN}" -H "Content-Type: application/json" -d "{ \"title\": \"${new_title}\"}"
